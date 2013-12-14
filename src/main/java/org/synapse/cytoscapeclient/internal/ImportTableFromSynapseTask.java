eacc756f7d720b64242116d20a0e3015708585db
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2013-12-13 16:42:13 -0800

    Further progress with table importing

--- src/main/java/org/synapse/cytoscapeclient/internal/ImportTableFromSynapseTask.java
+++ src/main/java/org/synapse/cytoscapeclient/internal/ImportTableFromSynapseTask.java
@@ -2,6 +2,7 @@ package org.synapse.cytoscapeclient.internal;
 
 import java.io.InputStream;
 import java.io.IOException;
+import org.cytoscape.io.read.CyTableReader;
 import org.cytoscape.io.read.CyTableReaderManager;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.work.AbstractTask;
@@ -25,49 +26,33 @@ public class ImportTableFromSynapseTask extends AbstractTask {
     if (entityId == null || entityId.length() == 0)
       return;
 
-    /*
     monitor.setTitle("Import table from Synapse");
     monitor.setStatusMessage("Getting entity information");
     final SynapseClient.File file = SynapseClient.get().getFile(entityId);
     fileContents = file.getContents();
 
     monitor.setStatusMessage("Reading Synapse file: " + file.getName());
-    final CyNetworkReader networkReader = networkReaderMgr.getReader(file.getContents(), file.getName());
-    if (networkReader == null)
-      throw new Exception("Unsupported network file type: " + file.getName());
+    final CyTableReader tableReader = tableReaderMgr.getReader(file.getContents(), file.getName());
+    if (tableReader == null)
+      throw new Exception("Unsupported table file type: " + file.getName());
 
-    super.insertTasksAfterCurrentTask(networkReader, new AbstractTask() {
+    super.insertTasksAfterCurrentTask(tableReader, new AbstractTask() {
       volatile boolean cancelled = false;
       public void run(TaskMonitor monitor) throws Exception {
-        for (final CyNetwork network : networkReader.getNetworks()) {
-          if (cancelled)
-            break;
-          
-          if (!networkMgr.networkExists(network.getSUID())) {
-            networkMgr.addNetwork(network);
-          }
-          if (!networkViewMgr.viewExists(network)) {
-            final CyNetworkView networkView = networkReader.buildCyNetworkView(network);
-            networkViewMgr.addNetworkView(networkView);
-          }
-        }
       }
 
       public void cancel() {
         cancelled = true;
       }
     });
-    */
   }
 
   public void cancel() {
-    /*
     cancelled = true;
     if (fileContents != null) {
       try {
         fileContents.close();
       } catch (IOException e) {}
     }
-    */
   }
 }

e4c6c3e8475208099cf6ebe2dd6a840a37a29274
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-01-31 14:38:28 -0800

    Fixed naming of networks

--- src/main/java/org/synapse/cytoscapeclient/internal/ImportNetworkFromSynapseTask.java
+++ src/main/java/org/synapse/cytoscapeclient/internal/ImportNetworkFromSynapseTask.java
@@ -3,10 +3,19 @@ package org.synapse.cytoscapeclient.internal;
 import java.io.File;
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.List;
+import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.task.read.LoadNetworkFileTaskFactory;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.Tunable;
 import org.cytoscape.work.TaskMonitor;
+import org.cytoscape.work.TaskObserver;
+import org.cytoscape.work.ObservableTask;
+import org.cytoscape.work.FinishStatus;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.subnetwork.CySubNetwork;
+import org.cytoscape.model.subnetwork.CyRootNetwork;
+
 
 public class ImportNetworkFromSynapseTask extends AbstractTask {
   final LoadNetworkFileTaskFactory loadNetworkFileTF;
@@ -16,7 +25,6 @@ public class ImportNetworkFromSynapseTask extends AbstractTask {
   public String entityId;
 
   volatile MaybeTask<SynClient.SynFile> task = null;
-  volatile boolean cancelled = false;
 
   public ImportNetworkFromSynapseTask(final LoadNetworkFileTaskFactory loadNetworkFileTF, final SynClientMgr clientMgr) {
     this.loadNetworkFileTF = loadNetworkFileTF;
@@ -40,7 +48,22 @@ public class ImportNetworkFromSynapseTask extends AbstractTask {
       return;
     }
 
-    super.insertTasksAfterCurrentTask(loadNetworkFileTF.createTaskIterator(file.getFile()));
+    super.insertTasksAfterCurrentTask(loadNetworkFileTF.createTaskIterator(file.getFile(), new TaskObserver() {
+      public void allFinished(final FinishStatus finishStatus) {}
+
+      public void taskFinished(final ObservableTask task) {
+        final List<CyNetworkView> views = (List<CyNetworkView>) task.getResults(List.class);
+        if (views.size() != 1) {
+          return;
+        }
+        final CyNetwork net = views.get(0).getModel();
+        net.getRow(net).set(CyNetwork.NAME, file.getName());
+        if (net instanceof CySubNetwork) {
+          final CyRootNetwork root = ((CySubNetwork) net).getRootNetwork();
+          root.getRow(root).set(CyNetwork.NAME, file.getName());
+        }
+      }
+    }));
   }
 
   public void cancel() {
